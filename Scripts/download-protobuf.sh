e8bb48b92fbd918cc4d7169a1ea3a8c5ddb9e434
jake <palatable7@naver.com>
2022-08-02 12:31:17 +0900

    Apply prefix

--- Scripts/download-protobuf.sh
+++ Scripts/download-protobuf.sh
@@ -1,11 +1,9 @@
-# download-protobuf.sh
-
 echo "Run Protobuf"
 cd Protobuf
 find . -name "*pb.swift" -exec rm {} \;
 protoc --swift_out=. ./*.proto
 
-echo "------------------------Success Generate-------------------------" # <-
+echo "------------------------Success Generate-------------------------"
 find . -name "*pb.swift" -exec ls {} \;
 echo "-----------------------------------------------------------------"
 find . -name "*pb.swift" -exec mv {} ../ExMyTest/Protobuf \;
